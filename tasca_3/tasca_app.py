ac1e8c32972e4ce13c7e4b76fb34b741c8a00ed4
Miguel Ángel Vílchez López <mangel@matronica.eu>
2024-01-05 14:19:33 +0100

    Llest

--- tasca_3/tasca_app.py
+++ tasca_3/tasca_app.py
@@ -3,15 +3,7 @@ from flask import Flask, jsonify, render_template, request
 
 app = Flask('tasta3-predict')
 
-# with open('models/regressio_logistica.pck', 'rb') as f:
-#     dv, model = pickle.load(f)
-
-# def predict_single(flor, dv, model):
-#     x = dv.transform([flor])
-#     y_pred = model.predict_proba(x)[:, 1]
-#     return y_pred[0]
-
-
+# Funció per a fer les peticions al model que vulguis
 def predice_modelo(flor, modelo):
     if modelo == "regresion":
         file = "models/regressio_logistica.pck"
@@ -33,25 +25,26 @@ def predice_modelo(flor, modelo):
     x = dv.transform([flor])
     y_pred = model.predict_proba(x)[:, 1]
     return y_pred[0]
-    
-    
+
 
 # Índex de la Web
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 # Predicció Web
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get input value from the form
+    
+    # Valors formatetgats com toca
     flor = {'petal amplada': float(request.form['amplada_value']), 'petal longitud': float(request.form['longitud_value'])}
-    # flor = [float(request.form['amplada_value']),float(request.form['longitud_value'])]
     modelo = request.form['modelo']
         
     prediction = predice_modelo(flor ,modelo)
 
     return render_template('result.html', amplada_value=request.form['amplada_value'], longitud_value=request.form['longitud_value'], modelo=modelo, prediction=prediction)
+
 
 # Directament amb CURL - Regression
 @app.route('/regresion', methods=['POST'])
@@ -68,6 +61,7 @@ def regresion():
 
     return jsonify(result)
 
+
 # Directament amb CURL - Decision Tree
 @app.route('/decision', methods=['POST'])
 def decision():
@@ -80,6 +74,7 @@ def decision():
 
     return jsonify(result)
 
+
 # Directament amb CURL - KNN
 @app.route('/knn', methods=['POST'])
 def knn():
@@ -91,6 +86,7 @@ def knn():
     }
 
     return jsonify(result)
+
 
 # Directament amb CURL - SVM
 @app.route('/svm', methods=['POST'])
